821ef4cba6d4e7ffbecab1876b399d96d12bd52c
LiudmylaTymoshenko <liuda.tymoshenko@gmail.com>
2026-01-29 18:31:09 -0500

    basic setup and auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log('DATABASE_URL (early) =', process.env.DATABASE_URL);
+
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
