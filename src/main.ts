00fe25d16d141d353020f825f96e6a9465cf65a1
LiudmylaTymoshenko <liuda.tymoshenko@gmail.com>
2026-03-16 19:17:44 -0400

    changed size limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '10mb' }));
 
   app.useGlobalPipes(
     new ValidationPipe({
